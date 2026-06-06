bb1aba47c0b185163924a6dbee2f5e3afe9d5309
Riley <speecil@speecil.dev>
2026-06-06 18:21:09 +1000

    replays: fix multi note hits not having accurate visuals

--- Assets/__Scripts/Previewer/MapControl/Objects/ObjectManager.cs
+++ Assets/__Scripts/Previewer/MapControl/Objects/ObjectManager.cs
@@ -602,6 +602,7 @@ public class ObjectManager : MonoBehaviour
                         newNote.wasMissed = false;
                         newNote.WasBadCut = matchingEvent.noteEventType == NoteEventType.bad;
                         newNote.HitOffset = matchingEvent.HitTimeOffset;
+                        newNote.HitTime = matchingEvent.Time;
                     }
                     matchingEvent?.SetEventValues(newNote.ScoringType, worldPosition);
 
@@ -670,6 +671,7 @@ public class ObjectManager : MonoBehaviour
                         newBomb.WasHit = true;
                         newBomb.WasBadCut = true;
                         newBomb.HitOffset = matchingEvent.ObjectTime - matchingEvent.Time;
+                        newBomb.HitTime = matchingEvent.Time;
 
                         Vector2 worldPosition = Instance.ObjectSpaceToWorldSpace(newBomb.Position);
                         matchingEvent.SetEventValues(ScoringType.NoScore, worldPosition);
@@ -789,6 +791,9 @@ public abstract class HitSoundEmitter : MapObject
     public bool wasMissed;
     public bool WasBadCut;
     public float HitOffset;
+    public float? HitTime;
+
+    public bool ShouldShowVisual => !WasHit || HitTime == null || TimeManager.CurrentTime < HitTime;
 }
 
 
